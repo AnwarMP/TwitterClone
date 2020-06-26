fb986e99c7fc7bc9a15ccff15e3bdd2f259caa5f
AnwarMP <ibnmujeeb2003@gmail.com>
2020-06-26 01:11:06 -0700

    first version of full app

--- client/client.js
+++ client/client.js
@@ -1,10 +1,15 @@
+
+
 console.log("hello world");
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const tweetsElement = document.querySelector('.tweets');
 const API_URL = 'http://localhost:5000/tweets';
 
 loadingElement.style.display = 'none';
+
+listAllTweets();
 
 form.addEventListener('submit',(event) =>{
     event.preventDefault();
@@ -27,5 +32,46 @@ form.addEventListener('submit',(event) =>{
         headers: {
             'content-type': 'application/json'
         }
-    });
+    }).then(response => response.json())
+      .then(createdTweet => {
+            console.log(createdTweet);
+            form.reset();
+            setTimeout(() =>{
+                form.style.display = '';
+
+            },30000);
+            form.style.display = '';
+            listAllTweets();
+            loadingElement.style.display = 'none';
+        });
 });
+
+function listAllTweets(){
+    tweetsElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(tweets => {
+           console.log(tweets);
+           tweets.reverse();
+           tweets.forEach(tweet => {
+                const div = document.createElement('div');
+                
+                const header = document.createElement('h3');
+                header.textContent = tweet.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = tweet.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(tweet.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                tweetsElement.appendChild(div);
+           });
+           loadingElement.style.display = 'none';
+
+        });
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
+const Filter = require('bad-words');
+const rateLimit = require("express-rate-limit");
 
 
 const app = express();
 
+const db = monk(process.env.MONGO_URI || 'localhost/twitter');
+const tweets = db.get('tweets');
+const filter = new Filter();
+
+
 app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.json({
@@ -14,8 +24,45 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/tweets',(req, res) => {
+    tweets
+        .find()
+        .then(tweets => {
+            res.json(tweets);
+        })
+})
+
+function isValidTweet(tweet){
+    return tweet.name && tweet.name.toString().trim() !== '' &&
+        tweet.content && tweet.content.toString().trim() !== '';
+}
+
+app.use(rateLimit({
+    windowMs: 30 * 1000,//30 seconds limiter one request
+    max:1
+}));
+
 app.post('/tweets', (req, res) =>{
-    console.log('req.body');
+    console.log(req.body);
+    if(isValidTweet(req.body)){
+        //insert to db
+        const tweet = {
+            name: filter.clean (req.body.name.toString()),
+            content: filter.clean(req.body.content.toString()),
+            created: new Date()
+        };
+
+        tweets
+            .insert(tweet)
+            .then(createdTweet => {
+                res.json(createdTweet);
+            });
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Please enter required fields'
+        })
+    }
     
 });
 
